98584b573819032cd29383a852e849a26e2e003c
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-08-02 10:04:30 +0800

    updating loader function to much leaner and readable code for the app component.

--- router/frontend/src/pages/Events.js
+++ router/frontend/src/pages/Events.js
@@ -12,3 +12,13 @@ function EventsPage() {
 }
 
 export default EventsPage;
+
+export const loader = async () => {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
