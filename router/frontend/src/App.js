c9d92fdfb1fca763bd30fe11a0ebc68ef3a63088
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-08-04 12:36:21 +0800

    fetching event data and adding functions on new event to event component.

--- router/frontend/src/App.js
+++ router/frontend/src/App.js
@@ -6,7 +6,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventsRoot from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
           },
           {
             path: ":eventId",
-            id: "event-detail",
+            id: "event-detail", //use definitions of route data useRouterLoader than useLoader.
             loader: eventDetailLoader,
             children: [
               {
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
--- router/frontend/src/pages/NewEvent.js
+++ router/frontend/src/pages/NewEvent.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { json, redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
 
 const NewEvent = () => {
   return (
     <>
-      <div>NewEvent</div>
+      <EventForm />
     </>
   );
 };
 
 export default NewEvent;
+
+export const action = async ({ request, params }) => {
+  const data = await request.formData();
+
+  let eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event details" }, { status: 500 });
+  }
+
+  return redirect("/events"); //redirecting to /events after submitting form
+};
