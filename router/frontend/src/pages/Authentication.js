5afa703e4a9ed09c6e8a7d6002cd443636eb4a66
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-08-11 15:31:14 +0800

    adding error message when validation user input, outputting validation errors.

--- router/frontend/src/pages/Authentication.js
+++ router/frontend/src/pages/Authentication.js
@@ -11,23 +11,16 @@ export default Authentication;
 export const action = async ({ request }) => {
   const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get("mode") || "login";
-  console.log(searchParams, "searchparams");
-  console.log(mode, "mode");
 
   if (mode !== "login" && mode !== "signup") {
     throw json({ message: "Unsupported mode." }, { status: 422 });
   }
-
-  console.log("unsupportedmode");
 
   const data = await request.formData();
   const authData = {
     email: data.get("email"),
     password: data.get("password"),
   };
-
-  console.log(data, "data");
-  console.log(authData, "authdata");
 
   const response = await fetch("http://localhost:8080/" + mode, {
     method: "POST",
@@ -37,19 +30,13 @@ export const action = async ({ request }) => {
     body: JSON.stringify(authData),
   });
 
-  console.log(response, "response");
-  console.log("stop");
-
   if (response.status === 422 || response.status === 401) {
     return response;
   }
-  console.log("responsestatus");
 
   if (!response.ok) {
     throw json({ message: "Could not authenticate User" }, { status: 500 });
   }
-
-  console.log("response got okay");
 
   return redirect("/");
 };
